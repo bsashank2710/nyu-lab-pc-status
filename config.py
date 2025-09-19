946efa930aabfacf1a5122a3813aed2d2119dd48
YOUR_REAL_NAME <YOUR_REAL_EMAIL>
2025-09-19 15:01:31 -0400

    Added SQLAlchemy database configuration

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+
+# Get the directory containing this file
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Database configuration
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'status_app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'status_app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Flask configuration
+DEBUG = True
+SECRET_KEY = 'dev'  # Change this to a random string in production!
